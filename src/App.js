04ca5bd8541ba67d8161a4a324f9b8993d22b0e8
hjkim0905 <eax9952@gmail.com>
2025-10-15 23:23:58 +0900

    chore: add [ERROR] text into the error message

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App {
     try {
       const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
       if (!input) {
-        throw new Error('빈 문자열입니다.');
+        throw new Error('[ERROR] 빈 문자열입니다.');
       }
     } catch (e) {
       Console.print(e.message);
