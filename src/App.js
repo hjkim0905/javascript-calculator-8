2dfd61a0fd4937d854b76c135a643268117aefe0
hjkim0905 <eax9952@gmail.com>
2025-10-16 20:31:50 +0900

    feat: parse custom separator & seprate input string by separator

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     let result = 0;
+    let arr = [];
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
     if (!input) {
       result = 0;
     } else {
-      let arr = input.split(/[,:]/);
+      const start = '//';
+      const end = '\\n';
+      const startIndex = input.indexOf(start);
+      const endIndex = input.indexOf(end);
+
+      if (startIndex !== -1 && endIndex !== -1) {
+        const delimiter = input.substring(startIndex + start.length, endIndex);
+
+        let replacedInput = input.replace(/\/|\\n/g, '');
+        arr = replacedInput.split(new RegExp(`[${delimiter},:]`));
+        Console.print(delimiter);
+        Console.print(arr);
+      } else {
+        arr = input.split(/[,:]/);
+      }
+
       for (let i = 0; i < arr.length; i++) {
-        arr[i] = parseInt(arr[i]);
-        result += arr[i];
+        const num = parseInt(arr[i]);
+        if (!isNaN(num)) {
+          result += num;
+        }
       }
     }
 
