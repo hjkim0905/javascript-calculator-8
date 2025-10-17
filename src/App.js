37d7d000268999ff89013c1b1fd4ccfffe6716df
hjkim0905 <eax9952@gmail.com>
2025-10-17 15:28:40 +0900

    refactor: error throw without try-catch statement

--- src/App.js
+++ src/App.js
@@ -6,56 +6,39 @@ class App {
     let arr = [];
     const input = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
-    try {
-      if (!input) {
-        result = 0;
-      } else if (!/\d/.test(input)) {
-        throw new Error('[ERROR] 입력된 문자열에 숫자가 포함되어있지 않습니다.');
+    if (!input) {
+      result = 0;
+    } else if (!/\d/.test(input)) {
+      throw new Error('[ERROR] 입력된 문자열에 숫자가 포함되어있지 않습니다.');
+    } else {
+      const start = '//';
+      const end = '\\n';
+      const startIndex = input.indexOf(start);
+      const endIndex = input.indexOf(end);
+
+      if (startIndex !== -1 && endIndex !== -1) {
+        const delimiter = input.substring(startIndex + start.length, endIndex);
+
+        let replacedInput = input.replace(/\/|\\n/g, '');
+        arr = replacedInput.split(new RegExp(`[${delimiter},:]`)).filter(Boolean);
       } else {
-        const start = '//';
-        const end = '\\n';
-        const startIndex = input.indexOf(start);
-        const endIndex = input.indexOf(end);
-
-        if (startIndex !== -1 && endIndex !== -1) {
-          const delimiter = input.substring(startIndex + start.length, endIndex);
-
-          let replacedInput = input.replace(/\/|\\n/g, '');
-          arr = replacedInput.split(new RegExp(`[${delimiter},:]`)).filter(Boolean);
-        } else {
-          arr = input.split(/[,:]/);
-          try {
-            for (let i = 0; i < arr.length; i++) {
-              if ((arr[i] !== ',' || arr[i] !== ':') && isNaN(arr[i])) {
-                throw new Error(
-                  '[ERROR] 입력값에 구분자로 사용될 수 없는 문자열이 포함되어있습니다.',
-                );
-              }
-            }
-          } catch (e) {
-            Console.print(e.message);
-            return;
+        arr = input.split(/[,:]/);
+        for (let i = 0; i < arr.length; i++) {
+          if ((arr[i] !== ',' || arr[i] !== ':') && isNaN(arr[i])) {
+            throw new Error('[ERROR] 입력값에 구분자로 사용될 수 없는 문자열이 포함되어있습니다.');
           }
         }
-        try {
-          for (let i = 0; i < arr.length; i++) {
-            const num = parseInt(arr[i]);
-
-            if (!Number.isNaN(num) && num > 0) {
-              result += num;
-            } else {
-              throw new Error('[ERROR] 입력된 문자열에 음수가 포함되어있습니다.');
-            }
-          }
-        } catch (e) {
-          Console.print(e.message);
-          return;
-        }
-        Console.print('결과 : ' + result);
       }
-    } catch (e) {
-      Console.print(e.message);
-      return;
+      for (let i = 0; i < arr.length; i++) {
+        const num = parseInt(arr[i]);
+
+        if (!Number.isNaN(num) && num > 0) {
+          result += num;
+        } else {
+          throw new Error('[ERROR] 입력된 문자열에 음수가 포함되어있습니다.');
+        }
+      }
+      Console.print('결과 : ' + result);
     }
   }
 }
