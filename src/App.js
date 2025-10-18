8d88a8507be47f34652222e98e3b53c6cd2e159e
hjkim0905 <eax9952@gmail.com>
2025-10-18 18:22:08 +0900

    refactor: remove else statements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
-import { DELIMITER } from './constants/DELIMITER.js';
+import { DELIMITER } from './constants/delimiter.js';
 import { REGEX } from './constants/regex.js';
 import { ERROR_MESSAGE } from './constants/error.js';
 import { MESSAGE } from './constants/message.js';
@@ -13,52 +13,66 @@ class App {
 
     if (!INPUT) {
       result = 0;
-    } else if (!REGEX.HAS_NUMBER.test(INPUT)) {
+      Console.print(MESSAGE.RESULT_PREFIX + result);
+      return;
+    }
+
+    if (!REGEX.HAS_NUMBER.test(INPUT)) {
       throw new Error(ERROR_MESSAGE.NO_NUMBER);
-    } else {
-      const START = DELIMITER.CUSTOM_PREFIX;
-      const START_INDEX = INPUT.indexOf(START);
-      const END_INDEX = INPUT.indexOf(DELIMITER.CUSTOM_SUFFIX);
+    }
 
-      if (START_INDEX !== -1 && END_INDEX !== -1) {
-        const DELIMITER = INPUT.substring(START_INDEX + START.length, END_INDEX);
-        const ESCAPED_DELIMITER = DELIMITER.replace(REGEX.ESCAPE_SPECIAL_CHAR, '\\$&');
-        const DELIMITER_SECTION = INPUT.substring(START_INDEX, END_INDEX + 2);
+    const START = DELIMITER.CUSTOM_PREFIX;
+    const START_INDEX = INPUT.indexOf(START);
+    const END_INDEX = INPUT.indexOf(DELIMITER.CUSTOM_SUFFIX);
 
-        let replacedInput = INPUT.replace(DELIMITER_SECTION, '');
-        if (
-          !(
-            replacedInput.includes(',') ||
-            replacedInput.includes(':') ||
-            (DELIMITER !== '' && replacedInput.includes(DELIMITER)) ||
-            REGEX.ONLY_NUMBER.test(replacedInput)
-          )
-        ) {
-          throw new Error(ERROR_MESSAGE.INVALID_DELIMITER);
-        }
+    if (START_INDEX !== -1 && END_INDEX !== -1) {
+      const DELIMITER = INPUT.substring(START_INDEX + START.length, END_INDEX);
+      const ESCAPED_DELIMITER = DELIMITER.replace(
+        REGEX.ESCAPE_SPECIAL_CHAR,
+        REGEX.ESCAPE_REPLACEMENT,
+      );
+      const DELIMITER_SECTION = INPUT.substring(START_INDEX, END_INDEX + 2);
 
-        splittedStrings = replacedInput
-          .split(new RegExp(`[${ESCAPED_DELIMITER},:]`))
-          .filter(Boolean);
-      } else {
-        splittedStrings = INPUT.split(DELIMITER.DEFAULT_REGEX);
-
-        splittedStrings.forEach((str) => {
-          if (str !== ',' && str !== ':' && isNaN(str)) {
-            throw new Error(ERROR_MESSAGE.INVALID_DELIMITER);
-          }
-        });
+      let replacedInput = INPUT.replace(DELIMITER_SECTION, '');
+      if (
+        !(
+          replacedInput.includes(',') ||
+          replacedInput.includes(':') ||
+          (DELIMITER !== '' && replacedInput.includes(DELIMITER)) ||
+          REGEX.ONLY_NUMBER.test(replacedInput)
+        )
+      ) {
+        throw new Error(ERROR_MESSAGE.INVALID_DELIMITER);
       }
 
-      const NUMBERS = splittedStrings.map(Number);
+      splittedStrings = replacedInput.split(new RegExp(`[${ESCAPED_DELIMITER},:]`)).filter(Boolean);
 
+      const NUMBERS = splittedStrings.map(Number);
       result = NUMBERS.reduce(function (acc, cur) {
         if (acc < 0) {
           throw new Error(ERROR_MESSAGE.NEGATIVE_NUMBER);
         }
         return acc + cur;
       });
+
+      Console.print(MESSAGE.RESULT_PREFIX + result);
+      return;
     }
+
+    splittedStrings = INPUT.split(DELIMITER.DEFAULT_REGEX);
+    splittedStrings.forEach((str) => {
+      if (str !== ',' && str !== ':' && isNaN(str)) {
+        throw new Error(ERROR_MESSAGE.INVALID_DELIMITER);
+      }
+    });
+
+    const NUMBERS = splittedStrings.map(Number);
+    result = NUMBERS.reduce(function (acc, cur) {
+      if (acc < 0) {
+        throw new Error(ERROR_MESSAGE.NEGATIVE_NUMBER);
+      }
+      return acc + cur;
+    });
     Console.print(MESSAGE.RESULT_PREFIX + result);
   }
 }
--- src/constants/regex.js
+++ src/constants/regex.js
@@ -2,4 +2,5 @@ export const REGEX = {
   HAS_NUMBER: /\d/,
   ONLY_NUMBER: /^[0-9]+$/,
   ESCAPE_SPECIAL_CHAR: /[.*+?^${}()|[\]\\]/g,
+  ESCAPE_REPLACEMENT: '\\$&',
 };
