0e071186ad3dd625810be866f17e7c171bd7fd46
hjkim0905 <eax9952@gmail.com>
2025-10-18 20:15:52 +0900

    refactor: integrate input/output logic into IOHandler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { Console } from '@woowacourse/mission-utils';
 import { calculateSum } from './domain/Calculator.js';
-import { MESSAGE } from './constants/message.js';
-import { printResult } from './domain/ResultHandler.js';
+import { readInput, printResult } from './domain/IOHandler.js';
 
 class App {
   async run() {
-    const INPUT = await Console.readLineAsync(MESSAGE.INPUT_PROMPT);
+    const INPUT = await readInput();
     const RESULT = calculateSum(INPUT);
     printResult(RESULT);
   }
--- src/domain/IOHandler.js
+++ src/domain/IOHandler.js
@@ -1,6 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
 import { MESSAGE } from '../constants/message.js';
 
+export function readInput() {
+  return Console.readLineAsync(MESSAGE.INPUT_PROMPT);
+}
+
 export function printResult(result) {
   Console.print(MESSAGE.RESULT_PREFIX + result);
 }
